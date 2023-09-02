7bba66147cb393d01367bf7c15eed6b16bd5e130
Oğuzhan <oguzhan.umutlu123@gmail.com>
2023-09-02 16:37:02 +0300

    builder fix, v1.0.6

--- tests/build.js
+++ tests/build.js
@@ -4,5 +4,5 @@ const uglify = require("uglify-js");
 writeFileSync(__dirname + "/../index.min.js", uglify.minify(readFileSync(__dirname + "/../index.js", "utf8")).code);
 writeFileSync(__dirname + "/../index.min.d.ts", readFileSync(__dirname + "/../index.d.ts", "utf8")
     .replaceAll(/[\n\t\v\r]/g, "")
-    .replaceAll(/\s*([:(){}<>]|=>)\s*/g, m => m.trim())
+    .replaceAll(/\s*([:(){}<>,]|=>)\s*/g, m => m.trim())
 );
